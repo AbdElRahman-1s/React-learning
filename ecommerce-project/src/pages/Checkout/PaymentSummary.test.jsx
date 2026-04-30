942e37f0914d6431d62da43f0f2764cd8376ca79
AbdElRahman <xinfargo@gmail.com>
2026-04-30 15:43:15 +0300

    create test for place order button

--- ecommerce-project/src/pages/Checkout/PaymentSummary.test.jsx
+++ ecommerce-project/src/pages/Checkout/PaymentSummary.test.jsx
@@ -3,7 +3,9 @@ import { PaymentSummary } from "./PaymentSummary";
 import { render, screen } from "@testing-library/react";
 import { vi } from "vitest";
 import { MemoryRouter } from "react-router";
+import { useLocation } from "react-router";
 import axios from "axios";
+import userEvent from "@testing-library/user-event";
 
 vi.mock('axios');
 
@@ -12,6 +14,7 @@ vi.mock('axios');
 describe('PaymentSummary integration test ', () => {
   let loadCart;
   let paymentSummary;
+  let user;
   beforeAll(() => {
     loadCart = vi.fn();//Mock
 
@@ -24,6 +27,7 @@ describe('PaymentSummary integration test ', () => {
       totalCostCents: 5251
     };
 
+    user=userEvent.setup();
 
   });
 
@@ -57,6 +61,33 @@ describe('PaymentSummary integration test ', () => {
     expect(
       screen.getByTestId('payment-summary-total')
     ).toHaveTextContent('$52.51');
+
+  });
+
+
+
+  it('clicks place order button ', async() => {
+  render(
+    <MemoryRouter>
+      <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+      <Location />
+    </MemoryRouter>
+  );
+
+function Location() {
+  const location = useLocation();
+
+  return(
+    <div data-testid="url-path">{location.pathname}</div>
+  );
+}
+
+const placeOrderButton = screen.getByTestId('place-order-button');
+  await user.click(placeOrderButton);
+  expect(axios.post).toHaveBeenCalledWith('/api/orders');
+expect(loadCart).toHaveBeenCalled();
+
+ expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
 
   });
 
